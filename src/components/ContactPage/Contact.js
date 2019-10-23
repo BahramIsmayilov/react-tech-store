29a295f119232792c6300edb69c81e8c84bd75ac
Bahram Ismayilov <bahram.ismayilov@hotmail.com>
2019-10-23 18:51:31 +0400

    change contact email adress

--- src/components/ContactPage/Contact.js
+++ src/components/ContactPage/Contact.js
@@ -9,7 +9,7 @@ export default function Contact() {
           <Title title="contact us" />
           <form
             className="mt-5"
-            action="https://formspree.io/REACT_APP_CONTACT_EMAIL"
+            action={`https://formspree.io/${process.env.REACT_APP_CONTACT_EMAIL}`}
             method="POST"
           >
             <div className="form-group">
